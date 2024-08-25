084a8b6af95e38a510204361b148be9b7c81d48a
amar-josh <amar.jadhav@joshsoftware.com>
2024-08-25 19:45:21 +0530

    updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,39 +2,30 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsparser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 
-export default {
-  ignores: ["dist"], // List files or directories to ignore
-  extends: [
-    js.configs.recommended,
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  parser: tsparser,
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-  },
-  files: ["**/*.{ts,tsx}"],
-  languageOptions: {
-    globals: globals.browser,
-  },
-  plugins: {
-    "react-hooks": reactHooks,
-    "react-refresh": reactRefresh,
-    "@typescript-eslint": tseslint,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-  },
-};
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      quotes: ["error", "double"], // Enforce double quotes
+      "space-infix-ops": ["error", { int32Hint: false }], // Enforce space around operators
+      semi: ["error", "always"], // Enforce semicolons
+    },
+  }
+);
